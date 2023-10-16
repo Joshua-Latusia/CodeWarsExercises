6ce0487b1841ba4dc55b7d02d92816d9fddd5617
Joshua Latusia <joshuaRMS@users.noreply.github.com>
2023-10-16 20:20:27 +0200

    Added test unit testing project and added APlusB kata sollution

---
+++ CodeWarsTests/8kyu/APlusBTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeWarsExercises;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWarsTests
+{
+    [TestClass]
+    public class APlusBTests
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            byte a = 1;
+            byte b = 2;
+            Assert.AreEqual(3, APlusB.Sum(a, b));
+        }
+    }
+}
